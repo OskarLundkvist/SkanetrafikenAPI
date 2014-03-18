90bbcab4dcf8087d4f4f0a12d20714633bff6da3
Jonas <mr_jonas86@hotmail.com>
2014-03-18 13:34:16 +0100

    Lagt till GUI

---
+++ SkanetrafikenAPI/src/SkanetrafikenGUI/GUI.java
@@ -0,0 +1,51 @@
+package SkanetrafikenGUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+
+public class GUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 200, 600);
+		contentPane.add(scrollPane);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(200, 0, 600, 600);
+		contentPane.add(lblNewLabel);
+	}
+}
