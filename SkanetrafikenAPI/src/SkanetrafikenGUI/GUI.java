5be81bacbb24108e826d27897c3e84ca939655ad
Jonas <mr_jonas86@hotmail.com>
2014-03-18 13:43:22 +0100

    Uppdaterat GUI med sökfält och knapp

--- SkanetrafikenAPI/src/SkanetrafikenGUI/GUI.java
+++ SkanetrafikenAPI/src/SkanetrafikenGUI/GUI.java
@@ -8,10 +8,15 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.Color;
 
 public class GUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -40,12 +45,23 @@ public class GUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 0, 200, 600);
-		contentPane.add(scrollPane);
-		
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setBounds(200, 0, 600, 600);
 		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBackground(Color.WHITE);
+		textField.setBounds(0, 0, 200, 61);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.setBounds(10, 72, 182, 23);
+		contentPane.add(btnSearch);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBackground(new Color(0, 153, 0));
+		textArea.setBounds(0, 0, 200, 600);
+		contentPane.add(textArea);
 	}
 }
