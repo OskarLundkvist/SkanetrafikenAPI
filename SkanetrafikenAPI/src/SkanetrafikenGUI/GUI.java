eb280ec20b2de5ee4300f72108b1879b7529ce36
Oskar Lundkvist <oskar.lundkvist@hotmail.com>
2014-03-20 16:20:47 +0100

    Lekt runt med en Jlist för att få sökresultaten att synas i infoboxen.

--- SkanetrafikenAPI/src/SkanetrafikenGUI/GUI.java
+++ SkanetrafikenAPI/src/SkanetrafikenGUI/GUI.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -20,10 +21,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
+import javax.swing.UIManager;
+import javax.swing.JTable;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+
 public class GUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
+	private JTable table;
+	private String[] stations = new String[2];
+	private Station station;
 
 	/**
 	 * Launch the application.
@@ -57,27 +66,43 @@ public class GUI extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBackground(Color.WHITE);
 		textField.setBounds(0, 0, 200, 61);
+		textField.setBackground(Color.WHITE);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnSearch = new JButton("Search");
+		btnSearch.setBounds(10, 72, 182, 23);
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ArrayList<Station> searchStations = new ArrayList<Station>(); 
 				searchStations.addAll(Parser.getStationsFromURL(textField.getText()));
-				for (Station s: searchStations){
-					System.out.println(s.getStationName());
+				for (int c = 0; c <= 2; c++){
+					stations[c] = new String (station.getStationName());
 				}
+				System.out.println(stations[0]);
+				System.out.println(stations[1]);
+				System.out.println(stations[2]);
 			}
 		});
-		btnSearch.setBounds(10, 72, 182, 23);
 		contentPane.add(btnSearch);
 		
 		JTextArea textArea = new JTextArea();
-		textArea.setBackground(new Color(0, 153, 0));
 		textArea.setBounds(0, 0, 200, 600);
+		textArea.setBackground(UIManager.getColor("DesktopIcon.borderRimColor"));
 		contentPane.add(textArea);
+		
+		JList list = new JList();
+		list.setModel(new AbstractListModel() {
+			public int getSize() {
+				return stations.length;
+			}
+			public Object getElementAt(int index) {
+				return stations[index];
+			}
+		});
+		list.setBounds(0, 93, 200, 307);
+		contentPane.add(list);
+		
 	}
 }
