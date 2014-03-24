c5a33f349778765f8b9525d31d25dfd115299b77
Alan Smithee <alan.smithee@example.com>
2014-03-24 16:25:40 +0100

    Hopefully fixed now...

--- SkanetrafikenAPI/src/SkanetrafikenGUI/GUI.java
+++ SkanetrafikenAPI/src/SkanetrafikenGUI/GUI.java
@@ -2,17 +2,35 @@ package SkanetrafikenGUI;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.DefaultListModel;
-import javax.swing.JScrollPane;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JTextArea;
-import javax.swing.JButton;
+import javax.swing.event.MouseInputListener;
+
+import org.jdesktop.swingx.JXMapViewer;
+import org.jdesktop.swingx.VirtualEarthTileFactoryInfo;
+import org.jdesktop.swingx.input.CenterMapListener;
+import org.jdesktop.swingx.input.PanKeyListener;
+import org.jdesktop.swingx.input.PanMouseInputListener;
+import org.jdesktop.swingx.input.ZoomMouseWheelListenerCenter;
+import org.jdesktop.swingx.mapviewer.DefaultTileFactory;
+import org.jdesktop.swingx.mapviewer.GeoPosition;
+import org.jdesktop.swingx.mapviewer.TileFactoryInfo;
+import org.jdesktop.swingx.mapviewer.WaypointPainter;
+import org.jdesktop.swingx.painter.CompoundPainter;
+import org.jdesktop.swingx.painter.Painter;
+
+import SkanetrafikenGUI.FancyWaypointRenderer;
+import SkanetrafikenGUI.MyWaypoint;
 
 import se.mah.k3lara.skaneAPI.model.Station;
 import se.mah.k3lara.skaneAPI.xmalparser.Parser;
@@ -68,6 +86,53 @@ public class GUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JXMapViewer mapViewer = new JXMapViewer();
+		mapViewer.setBounds(200, 0, 800, 600);
+		
+		contentPane.add(mapViewer);
+		
+		// Create a TileFactoryInfo for OpenStreetMap
+		TileFactoryInfo info = new VirtualEarthTileFactoryInfo(VirtualEarthTileFactoryInfo.MAP);
+		DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+		tileFactory.setThreadPoolSize(8);
+		mapViewer.setTileFactory(tileFactory);
+		
+		GeoPosition malmoCentralen = new GeoPosition(55.609147, 12.999034);
+		GeoPosition malmoNobeltorget = new GeoPosition(55.591298, 13.019252);
+
+		// Create a track from the geo-positions
+		List<GeoPosition> track = Arrays.asList(malmoCentralen, malmoNobeltorget);
+		RoutePainter routePainter = new RoutePainter(track);
+
+		// Set the focus
+		mapViewer.zoomToBestFit(new HashSet<GeoPosition>(track), 0.7);
+
+		// Create waypoints from the geo-positions
+		Set<MyWaypoint> waypoints = new HashSet<MyWaypoint>(Arrays.asList(
+				new MyWaypoint("A", Color.ORANGE, malmoCentralen),
+				new MyWaypoint("B", Color.RED, malmoNobeltorget)));
+
+		// Create a waypoint painter that takes all the waypoints
+		WaypointPainter<MyWaypoint> waypointPainter = new WaypointPainter<MyWaypoint>();
+		waypointPainter.setWaypoints(waypoints);
+		waypointPainter.setRenderer(new FancyWaypointRenderer());
+		
+		// Create a compound painter that uses both the route-painter and the waypoint-painter
+		List<Painter<JXMapViewer>> painters = new ArrayList<Painter<JXMapViewer>>();
+		painters.add(routePainter);
+		painters.add(waypointPainter);
+		
+		CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
+		mapViewer.setOverlayPainter(painter);
+		
+		// Add interactions
+		MouseInputListener mia = new PanMouseInputListener(mapViewer);
+		mapViewer.addMouseListener(mia);
+		mapViewer.addMouseMotionListener(mia);
+		mapViewer.addMouseListener(new CenterMapListener(mapViewer));
+		mapViewer.addMouseWheelListener(new ZoomMouseWheelListenerCenter(mapViewer));
+		mapViewer.addKeyListener(new PanKeyListener(mapViewer));
 		
 		textField = new JTextField();
 		textField.setBounds(0, 0, 200, 30);
